89e67603411745f23da9dda689517ec3b0f93eaf
GladysGhost <mukosibuilo@gmail.com>
2024-08-06 23:07:49 +0200

    yoh

---
+++ scripts.js
@@ -0,0 +1,10 @@
+// Hamburger menu toggle
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.querySelector('.hamburger');
+    const navLinks = document.querySelector('.nav-links');
+  
+    hamburger.addEventListener('click', () => {
+      navLinks.classList.toggle('show');
+    });
+  });
+  
